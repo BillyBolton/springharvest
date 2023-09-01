e43af9ced435ff51ca09c066f8cb648ee9796f29
billybolton <billybolton16@gmail.com>
2023-08-31 22:40:45 -0400

    initial commit

---
+++ src/main/java/ca/billybolton/rest_crud_library/RestCrudLibraryApplication.java
@@ -0,0 +1,13 @@
+package ca.billybolton.rest_crud_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestCrudLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/billybolton/rest_crud_library/RestCrudLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.billybolton.rest_crud_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
