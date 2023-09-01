82d71ae17faec099cd798489215a1ab0a6b49e2f
billybolton <billybolton16@gmail.com>
2023-09-01 00:25:07 -0400

    common

---
+++ common/src/main/java/ca/billybolton/common/models/domains/DomainModel.java
@@ -0,0 +1,10 @@
+package ca.billybolton.common.models.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@AllArgsConstructor
+public class DomainModel {
+}
+
---
+++ common/src/test/java/ca/billybolton/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.billybolton.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
