eb832048aba04896b0794b5eb72f2d761249e6fd
billybolton <billybolton16@gmail.com>
2023-09-02 14:08:55 -0400

    move components

---
+++ components/search/src/main/java/dev/springharvest/search/SearchApplication.java
@@ -0,0 +1,13 @@
+package dev.springharvest.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+
+}
