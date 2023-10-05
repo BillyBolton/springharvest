8ba3b845f9504bcbbda9aa53bf7f78a756af1790
billybolton <billybolton16@gmail.com>
2023-10-05 18:18:13 -0400

    clean up

--- components/components-testing/src/main/java/dev/springharvest/testing/integration/search/helpers/AbstractSearchTestFactoryImpl.java
+++ components/components-testing/src/main/java/dev/springharvest/testing/integration/search/helpers/AbstractSearchTestFactoryImpl.java
@@ -6,10 +6,8 @@ import dev.springharvest.search.model.queries.parameters.selections.SelectionDTO
 import dev.springharvest.search.model.queries.requests.filters.BaseFilterRequestDTO;
 import dev.springharvest.search.model.queries.requests.search.SearchRequestDTO;
 import dev.springhavest.common.models.domains.DomainModel;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.Getter;
@@ -27,39 +25,16 @@ public abstract class AbstractSearchTestFactoryImpl<M extends DomainModel, B ext
   }
 
   @Override
-  public Map<String, List<SelectionDTO>> buildValidSelections() {
-
-    Map<String, List<SelectionDTO>> selections = new HashMap<>();
-
-    selections.put("id", List.of(SelectionDTO.builder().alias(getIdPath()).build()));
-    selections.put("empty", List.of());
-    selections.put("all", buildValidSelections(false));
-
-    return selections;
-
-  }
-
-  @Override
-  public List<SelectionDTO> buildValidSelections(boolean selectAll) {
-
-    if (selectAll) {
-      return List.of();
-    }
-
-    return entityMetadata.getPaths().stream().map(path -> SelectionDTO.builder().alias(path).build()).collect(Collectors.toList());
-
+  public List<SelectionDTO> buildValidSelections(boolean isEmpty) {
+    return isEmpty ? List.of() : entityMetadata.getPaths().stream().map(path -> SelectionDTO.builder().alias(path).build()).collect(Collectors.toList());
   }
 
   @Override
   public List<SearchRequestDTO<B>> buildValidSearchRequestDTOs() {
 
     List<SearchRequestDTO<B>> requests = new LinkedList<>();
-
-    Map<String, List<SelectionDTO>> selections = buildValidSelections();
-
-    // TODO make parameterized. Only using the query that selects id for now.
     requests.add(SearchRequestDTO.<B>builder()
-                     .selections(selections.get("id"))
+                     .selections(buildValidSelections(false))
                      .filters(Set.of(buildValidFilters()))
                      .build());
 
