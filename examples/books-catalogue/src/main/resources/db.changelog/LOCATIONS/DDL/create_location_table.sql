d9e8e2c9e60578883a845b418f6144c81aa8a917
billybolton <billybolton16@gmail.com>
2023-11-11 02:27:39 -0500

    clean up

--- examples/books-catalogue/src/main/resources/db.changelog/LOCATIONS/DDL/create_location_table.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
---changeset BillyBolton:create-location-table
-
-CREATE TABLE location
-(
-    id          UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    coordinates geography(Point, 4326)
-) INHERITS (base_trace_parent);
