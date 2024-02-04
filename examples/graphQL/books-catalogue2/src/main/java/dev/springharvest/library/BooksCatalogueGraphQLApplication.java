cd15b3596b21bdc3226d2113798a16e6c6c74466
Gilles Djawa <72110273+NeroNemesis@users.noreply.github.com>
2024-02-04 15:53:44 -0500

    Revert "Structure"
    This reverts commit 77d07b31f58ed5a824b85d135c976dbd11ed55e9.

--- examples/graphQL/books-catalogue2/src/main/java/dev/springharvest/library/BooksCatalogueGraphQLApplication.java
+++
@@ -1,7 +0,0 @@
-package dev.springharvest.library;
-
-public class BooksCatalogueGraphQLApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
