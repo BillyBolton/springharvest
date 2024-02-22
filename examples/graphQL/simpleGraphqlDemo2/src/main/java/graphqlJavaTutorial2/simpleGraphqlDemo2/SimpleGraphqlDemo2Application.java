eacdb423b87c19249dc22bd7409cb4fa76fd515a
Moctar <moctar.wolf@gmail.com>
2024-02-21 21:30:14 -0500

    Integrating a postgreSQL-db to the demo

---
+++ examples/graphQL/simpleGraphqlDemo2/src/main/java/graphqlJavaTutorial2/simpleGraphqlDemo2/SimpleGraphqlDemo2Application.java
@@ -0,0 +1,13 @@
+package graphqlJavaTutorial2.simpleGraphqlDemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGraphqlDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleGraphqlDemo2Application.class, args);
+	}
+
+}
---
+++ examples/graphQL/simpleGraphqlDemo2/src/test/java/graphqlJavaTutorial2/simpleGraphqlDemo2/SimpleGraphqlDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package graphqlJavaTutorial2.simpleGraphqlDemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGraphqlDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
